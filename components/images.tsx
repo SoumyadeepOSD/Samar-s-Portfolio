16d93824bf84e4d0569a56f4be0cfad8e5f60920
SoumyadeepOSD <opencontainer255@gmail.com>
2024-12-01 15:23:09 +0530

    Code added

--- components/images.tsx
+++ components/images.tsx
@@ -1,30 +1,30 @@
-const profileImage = "/assets/profile_photo.png";
-const bachelorImage = "/assets/bachelor_image.jpg";
-const drivingImage = "/assets/driving_image.jpg";
 const higherSecondaryImage = "/assets/higher_secondary_image.jpg";
-const masterImage = "/assets/master_image.jpg";
-const panImage = "/assets/pan_image.jpg";
 const rationcardImage = "/assets/ration_card_image.jpg";
 const secondaryImage = "/assets/secondary_image.jpg";
-const kplogoImage = "/assets/kp_logo.jpeg";
-const aadhaarImage = "/assets/aadhaar_image.jpg";
-const voterImage = "/assets/voter_card.jpg";
 const vaccinationImage = "/assets/vaccine_image.jpg";
 const vehicleDocImages = "/assets/vehicle_image.jpg";
+const bachelorImage = "/assets/bachelor_image.jpg";
+const profileImage = "/assets/profile_photo.png";
+const drivingImage = "/assets/driving_image.jpg";
+const aadhaarImage = "/assets/aadhaar_image.jpg";
+const masterImage = "/assets/master_image.jpg";
+const voterImage = "/assets/voter_card.jpg";
+const kplogoImage = "/assets/kp_logo.jpeg";
+const panImage = "/assets/pan_image.jpg";
 
 
 export {
-    profileImage, 
-    bachelorImage, 
-    drivingImage, 
     higherSecondaryImage, 
-    masterImage, 
-    panImage, 
+    vehicleDocImages,
+    vaccinationImage,
     rationcardImage, 
     secondaryImage, 
-    kplogoImage,
+    bachelorImage, 
+    profileImage, 
+    drivingImage, 
     aadhaarImage,
+    kplogoImage,
+    masterImage, 
     voterImage,
-    vaccinationImage,
-    vehicleDocImages
+    panImage, 
 };
